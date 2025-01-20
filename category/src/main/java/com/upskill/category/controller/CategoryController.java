0fddf8c95613d62e0b925b26f9b757445fb07069
Khin Lei Wah Win <khinleiwahwin84@gmail.com>
2025-01-20 23:34:55 +0800

    Add category service

---
+++ category/src/main/java/com/upskill/category/controller/CategoryController.java
@@ -0,0 +1,33 @@
+package com.upskill.category.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.upskill.category.model.Category;
+import com.upskill.category.repository.CategoryRepository;
+
+@RestController
+@RequestMapping("api")
+public class CategoryController {
+	@Autowired
+	private Environment environment;
+	
+	@Autowired
+	private CategoryRepository repo;
+	
+	@GetMapping("/category/get_category")
+	public List<Category> retrieveExchangeValue() {
+		Category category = new Category();
+		String port = environment.getProperty("local.server.port");
+		System.out.println("env port "+ port);
+		
+		
+		return repo.findAll();
+	}
+
+}
